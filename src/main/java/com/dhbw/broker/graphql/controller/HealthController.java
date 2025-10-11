587c57b5dbecef4f2a34e1d6673bd73713c47c0d
DevMX808 <mx-dev@outlook.com>
2025-10-11 19:33:54 +0200

    formated health controller to object

--- src/main/java/com/dhbw/broker/graphql/controller/HealthController.java
+++ src/main/java/com/dhbw/broker/graphql/controller/HealthController.java
@@ -2,11 +2,12 @@ package com.dhbw.broker.graphql.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public Map<String, String> health() {
+        return Map.of("status", "OK");
     }
 }
