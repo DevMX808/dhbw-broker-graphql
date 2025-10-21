c89d7b3ea095f2a6bb990199de827a3b8a1bdb70
DevMX808 <mx-dev@outlook.com>
2025-10-21 17:40:17 +0200

    deleted queryresolver

--- src/main/java/com/dhbw/broker/graphql/api/QueryController.java
+++ src/main/java/com/dhbw/broker/graphql/api/QueryController.java
@@ -3,11 +3,18 @@ package com.dhbw.broker.graphql.api;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
+
 @Controller
 public class QueryController {
 
     @QueryMapping
     public String ping() {
         return "pong";
+    }
+
+    @QueryMapping
+    public Map<String, Object> me() {
+        return Map.of("id", "demo-user-1", "email", "demo@example.com");
     }
 }
